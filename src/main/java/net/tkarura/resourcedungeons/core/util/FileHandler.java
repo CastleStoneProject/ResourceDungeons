557c9c235e09316d9c91d2069adb7ecb3f82b0b8
thekarura <karura2310@yahoo.co.jp>
2016-05-08 23:50:35 +0900

    add util package and FileHandler.java file.

---
+++ src/main/java/net/tkarura/resourcedungeons/core/util/FileHandler.java
@@ -0,0 +1,36 @@
+package net.tkarura.resourcedungeons.core.util;
+
+import java.io.File;
+import java.io.FileFilter;
+
+/**
+ * ファイルに関する便利な機能を取り扱うクラス
+ * @author the_karura
+ */
+public final class FileHandler {
+	
+	private FileHandler() {}
+	
+	/**
+	 * ディレクトリフィルターです。
+	 * ディレクトリのみを条件に含めます。
+	 */
+	public final static FileFilter FOLDER_FILTER = new FileFilter() {
+		@Override
+		public boolean accept(File pathname) {
+			return pathname.isDirectory();
+		}
+	};
+	
+	/**
+	 * JavaScriptフィルターです。
+	 * JavaScript拡張子を持つファイルのみを条件に含めます。
+	 */
+	public final static FileFilter JAVA_SCRIPT_FILTER = new FileFilter() {
+		@Override
+		public boolean accept(File pathname) {
+			return pathname.getName().endsWith("js");
+		}
+	};
+	
+}
