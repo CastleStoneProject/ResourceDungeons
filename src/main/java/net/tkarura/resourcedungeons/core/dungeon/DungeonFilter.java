487902040a3f4f23c3a6188a75f429f1dc4ce8d2
thekarura <karura2310@yahoo.co.jp>
2016-06-20 06:21:47 +0900

    [add] DungeonFilterクラスのドキュメントを追加

--- src/main/java/net/tkarura/resourcedungeons/core/dungeon/DungeonFilter.java
+++ src/main/java/net/tkarura/resourcedungeons/core/dungeon/DungeonFilter.java
@@ -1,7 +1,18 @@
 package net.tkarura.resourcedungeons.core.dungeon;
 
+/**
+ * ダンジョン情報のフィルターです。
+ * 
+ * @author the_karura
+ */
 public interface DungeonFilter {
 	
+	/**
+	 * 引数で渡されたダンジョン情報がフィルターの条件に含まれているかを判別します。
+	 * 
+	 * @param dungeon フィルターにかけるダンジョン情報
+	 * @return ダンジョンがフィルターの条件に一致する場合trueを返します・
+	 */
 	public boolean accept(Dungeon dungeon);
 	
 }
