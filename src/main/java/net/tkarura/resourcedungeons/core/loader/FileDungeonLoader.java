1330138ec61c8f6d5df45a85502a1b61d9c0734f
thekarura <karura2310@yahoo.co.jp>
2016-12-30 14:29:45 +0900

    [add] Fileからの読み込み用のローダインタフェースを追加

---
+++ src/main/java/net/tkarura/resourcedungeons/core/loader/FileDungeonLoader.java
@@ -0,0 +1,39 @@
+package net.tkarura.resourcedungeons.core.loader;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import net.tkarura.resourcedungeons.core.dungeon.Dungeon;
+import net.tkarura.resourcedungeons.core.dungeon.IDungeon;
+import net.tkarura.resourcedungeons.core.exception.DungeonLoadException;
+import net.tkarura.resourcedungeons.core.loader.handle.LoadLogHandleList;
+
+public abstract class FileDungeonLoader implements DungeonLoader {
+
+    protected File file;
+    protected File dir;
+    protected Dungeon dungeon;
+    protected final LoadLogHandleList logs = new LoadLogHandleList();
+    
+    public IDungeon loadFileDungeon(File file) throws DungeonLoadException {
+	
+	this.file = file;
+	this.dir = file.getParentFile();
+	
+	try {
+    	
+	    return this.loadDungeon(new FileInputStream(file));
+    	
+	} catch (FileNotFoundException e) {
+	    throw new DungeonLoadException(e.getLocalizedMessage());
+	}
+	
+    }
+    
+    @Override
+    public LoadLogHandleList getLogHandleList() {
+	return this.logs;
+    }
+    
+}
