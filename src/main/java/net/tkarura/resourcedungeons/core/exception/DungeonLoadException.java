e837e44dcff6ef02cd24d72c85b4fe86d1639dbe
thekarura <karura2310@yahoo.co.jp>
2016-12-30 14:26:54 +0900

    [clean] 不要なインデントを解消

--- src/main/java/net/tkarura/resourcedungeons/core/exception/DungeonLoadException.java
+++ src/main/java/net/tkarura/resourcedungeons/core/exception/DungeonLoadException.java
@@ -3,21 +3,21 @@ package net.tkarura.resourcedungeons.core.exception;
 import net.tkarura.resourcedungeons.core.dungeon.IDungeon;
 
 public class DungeonLoadException extends DungeonException {
-    
+
     private static final long serialVersionUID = 1L;
 
     public DungeonLoadException() {
 	super();
     }
-    
+
     public DungeonLoadException(String message) {
 	super(message);
     }
-    
+
     public DungeonLoadException(IDungeon dungeon) {
 	super();
     }
-    
+
     public DungeonLoadException(String message, IDungeon dungeon) {
 	super(message);
     }
