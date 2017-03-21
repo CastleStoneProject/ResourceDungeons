04de2cf8e2e5b07319161d0e26c2cbddb4df5b10
thekarura <karura2310@yahoo.co.jp>
2017-03-22 07:29:37 +0900

    [update] Dungeon.class IDungeon.class にドキュメントを追加

--- src/main/java/net/tkarura/resourcedungeons/core/dungeon/Dungeon.java
+++ src/main/java/net/tkarura/resourcedungeons/core/dungeon/Dungeon.java
@@ -6,6 +6,11 @@ import java.util.List;
 
 import org.apache.commons.lang3.Validate;
 
+/**
+ * ダンジョンに関わる情報を保持する為のクラスです。
+ * 
+ * @author the_karura
+ */
 public class Dungeon implements IDungeon {
     
     protected final String id;
@@ -17,35 +22,68 @@ public class Dungeon implements IDungeon {
     protected List<DungeonUser> authors = new ArrayList<DungeonUser>();
     protected List<DungeonUser> contributors = new ArrayList<DungeonUser>();
     
+    /**
+     * ダンジョンIDを指定して生成します。
+     * ダンジョンIDは他のIDと被らない文字列を指定する必要があります。
+     * @param id ダンジョンID
+     */
     public Dungeon(String id) {
 	Validate.notNull(id, "id is null.");
 	this.id = id;
     }
     
+    /**
+     * ダンジョンの構造が配置されているディレクトリを設定します。
+     * @param dir ダンジョン構造
+     */
     public void setDirectory(File dir) {
 	this.dir = dir;
     }
     
+    /**
+     * ダンジョンのサポートされるバージョンを設定します。
+     * @param support サポートバージョン
+     */
     public void setSupport(String support) {
 	this.support = support;
     }
     
+    /**
+     * ダンジョンの名前を設定します。
+     * @param name ダンジョンの名前になる文字列
+     */
     public void setName(String name) {
 	this.name = name != null ? name : "N/A";
     }
     
+    /**
+     * ダンジョンのバージョンを設定します。
+     * @param version バージョン
+     */
     public void setVersion(String version) {
 	this.version = version != null ? version : "N/A";
     }
     
+    /**
+     * ダンジョンの説明を設定します。
+     * @param discription ダンジョンの説明
+     */
     public void setDiscription(String discription) {
 	this.discription = discription;
     }
     
+    /**
+     * ダンジョン作成に関わった作者のユーザ情報を追加します。
+     * @param user ユーザ情報
+     */
     public void addAuthor(DungeonUser user) {
 	this.authors.add(user);
     }
     
+    /**
+     * ダンジョン作成に関わった貢献者のユーザ情報を追加します。
+     * @param user 貢献者
+     */
     public void addContributor(DungeonUser user) {
 	this.contributors.add(user);
     }
--- src/main/java/net/tkarura/resourcedungeons/core/dungeon/IDungeon.java
+++ src/main/java/net/tkarura/resourcedungeons/core/dungeon/IDungeon.java
@@ -3,22 +3,59 @@ package net.tkarura.resourcedungeons.core.dungeon;
 import java.io.File;
 import java.util.List;
 
+/**
+ * ダンジョンに関わる情報を受け渡す為のインタフェースです。
+ * 
+ * @author the_karura
+ */
 public interface IDungeon {
     
+    /**
+     * ダンジョンの構造を保持しているディレクトリ情報を返します。
+     * @return ダンジョン構造を構成するファイルが格納されたディレクトリ
+     */
     public File getDirectory();
     
+    /**
+     * ダンジョンIDを返します。
+     * @return ダンジョンのユニークID
+     */
     public String getId();
     
+    /**
+     * ダンジョンのサポートバージョンを返します。
+     * @return サポートバージョン
+     */
     public String getSupport();
     
+    /**
+     * ダンジョンの名前を返します。
+     * @return ダンジョンの名前
+     */
     public String getName();
     
+    /**
+     * ダンジョンのバージョンを返します。
+     * @return バージョン
+     */
     public String getVersion();
     
+    /**
+     * ダンジョンの説明を返します。
+     * @return 説明
+     */
     public String getDiscription();
     
+    /**
+     * ダンジョンの作成に関わった製作者のユーザ情報一覧を返します。
+     * @return ユーザ情報一覧
+     */
     public List<DungeonUser> getAuthors();
     
+    /**
+     * ダンジョンの作成に関わった貢献者のユーザ情報一覧を返します。
+     * @return ユーザ情報一覧
+     */
     public List<DungeonUser> getContributors();
     
 }
