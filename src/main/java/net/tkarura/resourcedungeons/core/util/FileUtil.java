2a0548a44423842f9dd4c662a2e0e04edd7fbbe6
thekarura <karura2310@yahoo.co.jp>
2018-08-29 11:18:21 +0900

    ファイル操作関連のメソッドを追加
    ディレクトリを再帰的に処理をしやすくするメソッドを追加
    zipファイルを指定のディレクトリに展開するメソッドを追加

--- src/main/java/net/tkarura/resourcedungeons/core/util/FileUtil.java
+++ src/main/java/net/tkarura/resourcedungeons/core/util/FileUtil.java
@@ -1,10 +1,19 @@
 package net.tkarura.resourcedungeons.core.util;
 
+import org.apache.commons.lang3.Validate;
+
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Enumeration;
+import java.util.function.Consumer;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipInputStream;
 
 public final class FileUtil {
 
@@ -30,6 +39,78 @@ public final class FileUtil {
         } catch (IOException e) {
             return dir.getAbsolutePath();
         }
+    }
+
+    public static void attachDirectory(File dirs, Consumer<File> consumer) {
+
+        Validate.notNull(dirs, "dirs can not be null.");
+
+        for (File dir : dirs.listFiles()) {
+            if (dir.isDirectory()) {
+                attachDirectory(dir, consumer);
+            }
+            consumer.accept(dir);
+        }
+
+    }
+
+    public static void unZipFolder(ZipInputStream stream, File open_dir) throws IOException {
+
+        ZipEntry entry;
+        while ((entry = stream.getNextEntry()) != null) {
+
+            File dir = new File(open_dir, entry.getName());
+
+            if (entry.isDirectory()) {
+                dir.mkdirs();
+                continue;
+            }
+
+            try (FileOutputStream output = new FileOutputStream(dir)) {
+                int i;
+                while ((i = stream.read()) != -1) {
+                    output.write(i);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            stream.closeEntry();
+        }
+
+    }
+
+    public static void unZipFolder(ZipFile file, File open_dir) {
+
+        Enumeration<? extends ZipEntry> enumeration = file.entries();
+
+        while (enumeration.hasMoreElements()) {
+
+            ZipEntry entry = enumeration.nextElement();
+            File dir = new File(open_dir, entry.getName());
+
+            // ディレクトリの展開
+            if (entry.isDirectory()) {
+                dir.mkdirs();
+                continue;
+            }
+
+            // ファイルの中身を展開先にコピー
+            try (
+                FileOutputStream out_stream = new FileOutputStream(dir);
+                InputStream input_stream = file.getInputStream(entry)
+            ) {
+                byte[] buf = new byte[1024];
+                int size = 0;
+                while ((size = input_stream.read(buf)) != -1) {
+                    out_stream.write(buf, 0, size);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
     }
 
 }
